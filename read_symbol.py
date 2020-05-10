209bfb47dcee2f40d925ae340cacd6003415d6e0
aniketdhole7 <40766879+aniketdhole07@users.noreply.github.com>
2020-05-10 18:53:48 +0530

    read_symbol

---
+++ read_symbol.py
@@ -0,0 +1,21 @@
+import pickle
+
+def get_company_name():
+	company_name=[]
+	file = open('package/symbols.pickle', 'rb')
+	data = pickle.load(file)
+	file.close()
+	for i in data['company']:
+	  company_name.append(i)
+	company_name.append('Google')  
+	return company_name
+
+def get_company_security():
+	file = open('package/symbols.pickle', 'rb')
+	data = pickle.load(file)
+	file.close()
+	security=[]
+	for i in data['symbol']:
+	  security.append(i)
+	security.append('GOOGL')
+	return security
