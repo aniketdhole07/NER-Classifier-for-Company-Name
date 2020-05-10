b2500bbfe565fc24c0ee26aa7ec5ac8bd8c3e037
aniketdhole7 <40766879+aniketdhole07@users.noreply.github.com>
2020-05-10 19:08:10 +0530

    added comments

--- get_input.py
+++ get_input.py
@@ -1,21 +1,21 @@
-import token_word
-import read_symbol
-import json 
-from collections import defaultdict
+import token_word  #Used to get final_list
+import json  #Used to store output to json format
+from collections import defaultdict  #Used for appending in dictionary
 
-sentence="Apple, Amazon and Microsoft are reporting earnings after market close on April 30th."
-final_list=token_word.get_list(sentence)
-symbol=[]
+sentence="Apple, Amazon and Microsoft are reporting earnings after market close on April 30th." #input sentence
+final_list=token_word.get_list(sentence)  #returns the predictions  ('company_name','security')
+
+symbol=[]  #used to store output
 
 for i in range(len(final_list)):
-	symbol.append(final_list[i][1])
-print(symbol)	
+	symbol.append(final_list[i][1])  #used to add company_name security to output
 
-json_output={'security':None,'securities':None}
+
+json_output={'security':None,'securities':None}  #json format
 json_output = defaultdict(list)
 if(len(symbol)>0):
-	json_output['security']=symbol[0]
+	json_output['security']=symbol[0]  #adding first occurance to output dict
 	for i in symbol:
-		json_output['securities'].append(i)
-with open('output.txt', 'w') as json_file:
-  json.dump(json_output, json_file)
+		json_output['securities'].append(i)   #adding all occurance to output dict
+with open('output.json', 'w') as json_file:
+  json.dump(json_output, json_file)  
