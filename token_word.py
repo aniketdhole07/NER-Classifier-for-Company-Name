986faa4e769b80f023c14183e859cffa505e82be
aniketdhole7 <40766879+aniketdhole07@users.noreply.github.com>
2020-05-11 17:56:12 +0530

    Solved The Issue
    1. Before we would get multiple companies for single entity,now it outputs only single company.I finds max value from all fuzz value for a certain entity
    
    2.Updated fuzz value to 55

--- token_word.py
+++ token_word.py
@@ -1,3 +1,5 @@
+
+
 import spacy 
 import pickle
 from fuzzywuzzy import fuzz  #fuzz value to check similarity between two strings
@@ -5,21 +7,26 @@ import read_symbol
 
 
 def get_list(sentence):
-	MIN_FUZZ_VALUE=65   #fuzz value to check similarity between two strings
+	MIN_FUZZ_VALUE=55   #fuzz value to check similarity between two strings
 	nlp = spacy.load('en_core_web_sm')  #pretrained model
 	company_name=read_symbol.get_company_name()  # list containing company_name
 	security=read_symbol.get_company_security()  # list containing security symbols
 	final_list=[]  # list containing output list (company_name,security)
-
+ 
 	doc = nlp(sentence)   #used to find entity
 	for ent in doc.ents: 
-		if(ent.label_=="ORG"):  #if entity is of an organization(company)
+		temp_fuzz_value=[]  #used to store all fuzz values
 
-			for indx in range(len(company_name)):
-				fuzz_value=fuzz.ratio(ent.text,company_name[indx])
-				
-				if(fuzz_value>=MIN_FUZZ_VALUE):
-					final_list.append([ent.text,security[indx]]) 
+		#Now it checks the maximum fuzz value and outputs only single company instead of multiple
+		for indx in range(len(company_name)):
+			temp_fuzz_value.append(fuzz.ratio(ent.text,company_name[indx]))
+			
+		max_fuzz_value=max(temp_fuzz_value)	 #Find maximum fuzz value to get best results
+		company_index=temp_fuzz_value.index(max_fuzz_value)
+		
+		if(max_fuzz_value>=MIN_FUZZ_VALUE):
+			#print(ent.text)
+			final_list.append([ent.text,security[company_index]]) 
 
 	return final_list				
 
